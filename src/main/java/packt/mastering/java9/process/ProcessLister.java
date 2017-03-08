645a55ac865fc093f763982c7177dcdf6aa2e65a
Peter Verhas <peter@verhas.com>
2017-03-08 17:52:06 +0100

    StackWalker example is created

--- src/main/java/packt/mastering/java9/process/ProcessLister.java
+++ src/main/java/packt/mastering/java9/process/ProcessLister.java
@@ -1,11 +1,9 @@
 package packt.mastering.java9.process;
 
+import packt.mastering.java9.stackwalker.DumpStack;
 import packt.mastering.java9.stackwalker.LogSetter;
 
 import java.lang.System.Logger;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.SimpleFormatter;
 
 public class ProcessLister {
     private static final Logger LOG = System.getLogger(ProcessLister.class.getName());
@@ -14,12 +12,13 @@ public class ProcessLister {
         LogSetter.console();
     }
 
-    private static final void debug(String format, Object ...message) {
+    private static void debug(String format, Object... message) {
         LOG.log(Logger.Level.DEBUG, format, message);
     }
 
     public static void main(String[] args) {
         LOG.log(Logger.Level.DEBUG, "starting");
+        DumpStack.dump(System.out);
         ProcessHandle.allProcesses().map(ProcessHandle::info).forEach(
                 (info) -> debug("{0} {1}", info.user().orElse("nobody"),
                         info.command().orElse(info.commandLine().orElse(" noname-process")))
--- src/main/java/packt/mastering/java9/stackwalker/LogSetter.java
+++ src/main/java/packt/mastering/java9/stackwalker/LogSetter.java
@@ -5,15 +5,15 @@ import java.util.logging.Level;
 import java.util.logging.SimpleFormatter;
 
 public class LogSetter {
-    public static void console(){
-        StackWalker walker = StackWalker.getInstance();
-
-        java.util.logging.Logger logger = java.util.logging.Logger.getLogger(walker.getCallerClass().getName());
+    public static void console() {
+        java.util.logging.Logger logger =
+                java.util.logging.Logger.getLogger(
+                        StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
+                                .getCallerClass().getName());
         logger.setLevel(Level.ALL);
         ConsoleHandler handler = new ConsoleHandler();
         handler.setLevel(Level.ALL);
         handler.setFormatter(new SimpleFormatter());
         logger.addHandler(handler);
-
     }
 }
